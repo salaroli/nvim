cad087f780cb607f8956c7f375cd15eed1d84d22
Fernando Salaroli <fernando@salaro.li>
2026-05-03 16:14:54 -0300

    arrumando blinky

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,35 +1,34 @@
 return {
-  "saghen/blink.cmp",
-  event = "InsertEnter",
-  version = "1.*",
-  dependencies = {
-    "rafamadriz/friendly-snippets",
-  },
-  opts = {
-    keymap = {
-      preset = "default",
-      ["<Tab>"] = { "snippet_forward", "select_next", "fallback" },
-      ["<S-Tab>"] = { "snippet_backward", "select_prev", "fallback" },
-      ["<CR>"] = { "accept", "fallback" },
-    },
-    appearance = {
-      nerd_font_variant = "mono",
-    },
-    completion = {
-      trigger = {
-        show_on_insert_on_trigger_character = true,
-        show_after_delay_ms = 0,
-      },
-      list = { selection = { preselect = true, auto_insert = false } },
-      menu = { auto_show = true },
-      documentation = { auto_show = true, auto_show_delay_ms = 200 },
-      ghost_text = { enabled = true },
-    },
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-    },
-    signature = { enabled = true },
-    fuzzy = { implementation = "prefer_rust_with_warning" },
-  },
-  opts_extend = { "sources.default" },
+	"saghen/blink.cmp",
+	event = "InsertEnter",
+	version = "1.*",
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+	},
+	opts = {
+		keymap = {
+			preset = "default",
+			["<Tab>"] = { "snippet_forward", "select_next", "fallback" },
+			["<S-Tab>"] = { "snippet_backward", "select_prev", "fallback" },
+			["<CR>"] = { "accept", "fallback" },
+		},
+		appearance = {
+			nerd_font_variant = "mono",
+		},
+		completion = {
+			trigger = {
+				show_on_insert_on_trigger_character = true,
+			},
+			list = { selection = { preselect = true, auto_insert = false } },
+			menu = { auto_show = true },
+			documentation = { auto_show = true, auto_show_delay_ms = 200 },
+			ghost_text = { enabled = true },
+		},
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+		signature = { enabled = true },
+		fuzzy = { implementation = "prefer_rust_with_warning" },
+	},
+	opts_extend = { "sources.default" },
 }
